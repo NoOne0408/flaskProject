307c3357f09986536f4bf530e2eb86f78bde95bf
xxx <499712308@qq.com>
2021-08-25 16:28:57 +0800

    change format

--- app.py
+++ app.py
@@ -4,11 +4,10 @@ import datetime
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def hello_world():  # put application's code here
 
-    roomType = ['一居室', '一室一厅', '两室一厅', '两室两厅', '三室一厅', '三室两厅', '三室一厅', '三室两厅']
+    roomType = ['1居室', '1室1厅', '2室1厅', '2室2厅', '3室1厅', '3室2厅', '4室1厅', '4室2厅']
     dict = {'name': 'zhangsan'}
     years = build_time_scope()
     return render_template('city.html', roomTypeList=roomType, dict=dict, buildTimeList=years)
@@ -18,11 +17,8 @@ def hello_world():  # put application's code here
 def build_time_scope():
     years = []
     year = datetime.datetime.now().year
-    while year >= 1980:
-        if (year == datetime.datetime.now().year):
-            interval = str(year - 10) + "~至今"
-        else:
-            interval = str(year - 10) + "~" + str(year)
+    while year - 10 >= 1980:
+        interval = str(year - 10) + "-" + str(year)
         years.append(interval)
         print(interval)
         year -= 10
@@ -32,13 +28,22 @@ def build_time_scope():
 @app.route('/prarmsSubmit/', methods=['POST', 'GET'])
 def prarmsSubmit():
     address1 = request.args.get('address1')
-    address2 = request.args.get('address2')
+    address2 = request.args.get('address2').split("区")[0]
     roomType = request.args.get('roomType')
+    floorType = request.args.get('floorType')
     buildTime = request.args.get('buildTime')
     area = request.args.get('area')
-    print(address1 + address2 + roomType + buildTime + area)
+
+    dict = {"area": [area], "floor": [floorType],
+            "build_time": [buildTime], "location": [address1 + "-" + address2], "room_shape": [roomType]}
+
+    print(dict)
+
     s = "10000"
     return s
+
+
+
 
 
 if __name__ == '__main__':
